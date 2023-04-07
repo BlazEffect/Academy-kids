c6775e999e586cf36a19c66f10ca15841bda3f43
makasinui <mks.shi@ya.ru>
2023-04-07 16:50:22 +0300

    init react

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
+
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react'
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
       refresh: true,
     }),
+    react()
   ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    rollupOptions: {
+        input: 'resources/js/app.js',
+    },
+},
   css: {
     devSourcemap: true,
   },
