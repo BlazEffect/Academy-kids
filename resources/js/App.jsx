889c03586c4f464e908748663e6f2e9ed08bfca9
makasinui <mks.shi@ya.ru>
2023-04-07 19:25:32 +0300

    wip

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
-export default function App(){
-    return(
-        <h1>How To Install React in Laravel 9 with Vite</h1>
-    );
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import router from './router/index';
 
 if(document.getElementById('root')){
-    createRoot(document.getElementById('root')).render(<App />)
+    createRoot(document.getElementById('root')).render(
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    )
 }
